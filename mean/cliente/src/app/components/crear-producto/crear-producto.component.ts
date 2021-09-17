4da5a4af16bd7abfd388e96d4649c8eb6cc663e0
JavierGold <567498jav@gmail.com>
2021-09-17 18:42:16 -0500

    add ReactiveFormsModule

--- mean/cliente/src/app/components/crear-producto/crear-producto.component.ts
+++ mean/cliente/src/app/components/crear-producto/crear-producto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-producto',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearProductoComponent implements OnInit {
 
-  constructor() { }
+  productoForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { 
+    this.productoForm = this.fb.group({
+      producto: ['',Validators.required],
+      categoria: ['',Validators.required],
+      ubicacion: ['',Validators.required],
+      precio: ['',Validators.required] 
+    })
+  }
 
   ngOnInit(): void {
+    console.log(this.productoForm)
+  }
+
+  agregarProducto() {
+
+    console.log("Agregar producto")
+
+  
   }
 
 }
