a568db7433c78d6aa580b3607a35d7913476ec33
JavierGold <567498jav@gmail.com>
2021-09-18 13:02:49 -0500

    DB connection

--- mean/servidor/index.js
+++ mean/servidor/index.js
@@ -1,7 +1,16 @@
 const express=require('express');
+const conectarDB = require('./config/db');
 
 // Crear servidor
 const app = express();
+
+//Conectamos a la BD
+conectarDB();
+
+//Definimos ruta principal
+app.get('/',(req,res)=>{
+    res.send("hola mundo")
+})
 
 app.listen(4000,()=>{
     console.log("Servidor funcionando correctamente")
