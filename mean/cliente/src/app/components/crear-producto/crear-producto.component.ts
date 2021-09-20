f981fb789e851181126df4e5647bc425a91b31fb
JavierGold <567498jav@gmail.com>
2021-09-20 17:22:53 -0500

    cambios finales

--- mean/cliente/src/app/components/crear-producto/crear-producto.component.ts
+++ mean/cliente/src/app/components/crear-producto/crear-producto.component.ts
@@ -1,6 +1,8 @@
+
+   
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Producto } from 'src/app/models/producto';
 import { ProductoService } from 'src/app/services/producto.service';
@@ -11,24 +13,25 @@ import { ProductoService } from 'src/app/services/producto.service';
   styleUrls: ['./crear-producto.component.css']
 })
 export class CrearProductoComponent implements OnInit {
-
   productoForm: FormGroup;
-
+  titulo = 'Crear producto';
+  id: string | null;
   constructor(private fb: FormBuilder,
               private router: Router,
               private toastr: ToastrService,
-              private _productoService: ProductoService
-              ) { 
+              private _productoService: ProductoService,
+              private aRouter: ActivatedRoute) { 
     this.productoForm = this.fb.group({
-      producto: ['',Validators.required],
-      categoria: ['',Validators.required],
-      ubicacion: ['',Validators.required],
-      precio: ['',Validators.required] 
+      producto: ['', Validators.required],
+      categoria: ['', Validators.required],
+      ubicacion: ['', Validators.required],
+      precio: ['', Validators.required],
     })
+    this.id = this.aRouter.snapshot.paramMap.get('id');
   }
 
   ngOnInit(): void {
-    
+    this.esEditar();
   }
 
   agregarProducto() {
@@ -37,9 +40,10 @@ export class CrearProductoComponent implements OnInit {
       nombre: this.productoForm.get('producto')?.value,
       categoria: this.productoForm.get('categoria')?.value,
       ubicacion: this.productoForm.get('ubicacion')?.value,
-      precio: this.productoForm.get('precio')?.value
+      precio: this.productoForm.get('precio')?.value,
     }
 
+    console.log(PRODUCTO);
     this._productoService.guardarProducto(PRODUCTO).subscribe(data => {
       this.toastr.success('El producto fue registrado con exito!', 'Producto Registrado!');
       this.router.navigate(['/']);
@@ -47,6 +51,23 @@ export class CrearProductoComponent implements OnInit {
       console.log(error);
       this.productoForm.reset();
     })
+
+  
+  }
+
+  esEditar() {
+
+    if(this.id !== null) {
+      this.titulo = 'Editar producto';
+      this._productoService.obtenerProducto(this.id).subscribe(data => {
+        this.productoForm.setValue({
+          producto: data.nombre,
+          categoria: data.categoria,
+          ubicacion: data.ubicacion,
+          precio: data.precio,
+        })
+      })
+    }
   }
 
 }
