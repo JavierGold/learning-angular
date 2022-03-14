e9ef992bb8a4a5e037409b49f0e9221ef6237b39
JavierGold <567498jav@gmail.com>
2022-03-14 17:16:19 -0600

    add: peliculas component

--- frameworks_master/AprendiendoAngular/src/app/app.routing.ts
+++ frameworks_master/AprendiendoAngular/src/app/app.routing.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from './components/home/home.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
-//import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
 import { PaginaComponent } from './components/pagina/pagina.component';
 import { ErrorComponent } from './components/error/error.component';
 
@@ -14,6 +14,7 @@ const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'blog', component: BlogComponent },
+    { path: 'peliculas', component: PeliculasComponent },
     { path: 'pagina-de-pruebas', component: PaginaComponent },
     { path: 'pagina-de-pruebas/:nombre/:apellidos', component: PaginaComponent },
     { path: 'formulario', component: FormularioComponent },
