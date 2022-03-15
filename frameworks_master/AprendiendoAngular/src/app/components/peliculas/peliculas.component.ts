7379112b7e8b31f81a2f670314da4e1c12abd500
JavierGold <567498jav@gmail.com>
2022-03-15 09:31:51 -0600

    add: Model Pelicula

--- frameworks_master/AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
+++ frameworks_master/AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Pelicula } from '../../models/pelicula';
 
 @Component({
   selector: 'app-peliculas',
@@ -7,26 +8,14 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class PeliculasComponent implements OnInit {
 
-  public peliculas: Array<any>
+  public peliculas: Array<Pelicula>;
 
   constructor() {
 
     this.peliculas = [
-      {
-        year:2019,
-        title: "Spiderman",
-        image: 'https://dam.smashmexico.com.mx/wp-content/uploads/2017/05/spider-man-3-y-su-edicion-alternativa-cover.jpg'
-      },
-      {
-        year:2018,
-        title: "Avengers",
-        image: 'https://ichef.bbci.co.uk/news/640/cpsprodpb/BF0D/production/_106090984_2e39b218-c369-452e-b5be-d2476f9d8728.jpg'
-      },
-      {
-        year:2015,
-        title: "Batman",
-        image: 'https://imagenes.elpais.com/resizer/c222sh89e2kpFZETxd208IGztAw=/1960x1103/cloudfront-eu-central-1.images.arcpublishing.com/prisa/Z7OXACA63JB57PNTW4S4BFJQCQ.jpg'
-      }, 
+      new Pelicula("Spiderman", 2019, "https://dam.smashmexico.com.mx/wp-content/uploads/2017/05/spider-man-3-y-su-edicion-alternativa-cover.jpg"),
+      new Pelicula("Los vengadores Endgame", 2018, "http://as01.epimg.net/tikitakas/imagenes/2019/04/26/portada/1556258369_131914_1556258703_noticia_normal.jpg"),
+      new Pelicula("Batman", 2015, "https://imagenes.elpais.com/resizer/c222sh89e2kpFZETxd208IGztAw=/1960x1103/cloudfront-eu-central-1.images.arcpublishing.com/prisa/Z7OXACA63JB57PNTW4S4BFJQCQ.jpg")
 
     ]
 
