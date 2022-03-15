469c13bf6733b9851e2f67711bf113ecf2be056c
JavierGold <567498jav@gmail.com>
2022-03-14 18:54:23 -0600

    add: directivas

--- frameworks_master/AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
+++ frameworks_master/AprendiendoAngular/src/app/components/peliculas/peliculas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-peliculas',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeliculasComponent implements OnInit {
 
-  constructor() { }
+  public peliculas: Array<any>
+
+  constructor() {
+
+    this.peliculas = [
+      {
+        year:2019,
+        title: "Spiderman",
+        image: 'https://dam.smashmexico.com.mx/wp-content/uploads/2017/05/spider-man-3-y-su-edicion-alternativa-cover.jpg'
+      },
+      {
+        year:2018,
+        title: "Avengers",
+        image: 'https://ichef.bbci.co.uk/news/640/cpsprodpb/BF0D/production/_106090984_2e39b218-c369-452e-b5be-d2476f9d8728.jpg'
+      },
+      {
+        year:2015,
+        title: "Batman",
+        image: 'https://imagenes.elpais.com/resizer/c222sh89e2kpFZETxd208IGztAw=/1960x1103/cloudfront-eu-central-1.images.arcpublishing.com/prisa/Z7OXACA63JB57PNTW4S4BFJQCQ.jpg'
+      }, 
+
+    ]
+
+  }
 
   ngOnInit(): void {
+
+    console.log(this.peliculas?.length)
   }
 
 }
